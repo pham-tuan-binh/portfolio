ea273e3b8a84fa443db291235fa0bc0add0049c8
Pham Tuan Binh <phamtuanbinh@MacBook-Pro-2.local>
2024-01-23 02:11:25 +0100

    Fix gltf include error

--- src/threejs/components/avatar.js
+++ src/threejs/components/avatar.js
@@ -1,10 +1,11 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { setupModel } from "./setupModel.js";
+import scene from "../models/scene.gltf";
 
 async function loadAvatar() {
   const loader = new GLTFLoader();
 
-  const avatar = await loader.loadAsync("/src/threejs/models/scene.gltf");
+  const avatar = await loader.loadAsync(scene);
 
   return setupModel(avatar);
 }
